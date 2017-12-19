41f4a6a9d7cba82c849f9c013801b34cc7fd0ad6
Łukasz Rosik <lukiro96@gmail.com>
2017-12-19 10:35:03 +0100

    logowanie uzytkownika

--- BDProj/BDProj/FormMain.cs
+++ BDProj/BDProj/FormMain.cs
@@ -12,7 +12,7 @@ namespace BDProj
 {
     public partial class FormMain : Form
     {
-        DataLibraryDataContext context = new DataLibraryDataContext();
+        private DataLibraryDataContext context = new DataLibraryDataContext();
         public FormMain()
         {
             InitializeComponent();
@@ -22,6 +22,22 @@ namespace BDProj
         {
             var borrows = from wypozyczenia in context.wypozyczenias select wypozyczenia;
             dataGridViewShowData.DataSource = borrows;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            var user = (from usr in context.Users
+                where usr.Email == textBoxLogin.Text && usr.Password == textBoxPassword.Text
+                select usr.Email).FirstOrDefault();
+
+            if (user != null)
+            {
+                MessageBox.Show($"Witaj użytkowniku {user}", "OK!");
+            }
+            else
+            {
+                MessageBox.Show("Niepoprawny login lub hasło.", "Błąd!");
+            }
         }
     }
 }
